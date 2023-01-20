93752936b2904f597df637e6b5f88ed7964cf11c
fat83dotcom <fernando.quimic@gmail.com>
2023-01-19 21:29:00 -0300

    Insetion Sort - Com preenchimento de vetor com numero aleatorios

--- CODIGOS_C/tema_4/isertionSort.c
+++ CODIGOS_C/tema_4/isertionSort.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-void insertionSort(int *vet, int tamVet){
+void insertionSort(int *vetor, int tamanhoVetor){
     int i, j, chave;
-    for ( j = 1; j < tamVet; j++){
-        chave = vet[j];
+    for ( j = 1; j < tamanhoVetor; j++){
+        chave = vetor[j];
         i = j - 1;
-        while (i >= 0 && vet[i] > chave){
-            vet[i + 1] = vet[i];
+        while (i >= 0 && vetor[i] > chave){
+            vetor[i + 1] = vetor[i];
             i--;
         }
-        vet[i + 1] = chave;
+        vetor[i + 1] = chave;
     }
 }
 
 int main(int argc, char const *argv[]){
-    int vet[10] = {3, 6, 9, 12, 1, 89, 4, 76, 323, 0};
+    const int tamanhoVetor = 100000;
+    int vetor[tamanhoVetor];
+    srand(time(NULL));
 
-    insertionSort(vet, 10);
+    for (int i = 0; i < tamanhoVetor; i++){
+        vetor[i] = rand() % 100000;
+    }
+    
+    insertionSort(vetor, tamanhoVetor);
 
-    for (int i = 0; i < 10; i++){
-        printf("%d\n", vet[i]);
+    for (int i = 0; i < tamanhoVetor; i++){
+        printf("%d\n", vetor[i]);
     }
     
     return 0;
