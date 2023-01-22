beb71a7aadb088fa8fd84986eb27ca34dcfd6b9a
fat83dotcom <fernando.quimic@gmail.com>
2023-01-22 20:38:39 -0300

    Script que compara o tempo de execução de diversos algoritmos - Ajuste de nomenclatura das funçoes

--- CODIGOS_C/tema_4/comparacaoAlgoritmos.c
+++ CODIGOS_C/tema_4/comparacaoAlgoritmos.c
@@ -58,6 +58,7 @@ void selectionSort(int *vetor, int tamanhoVetor){
 
 void preenchedorVetor(int *vetor, int tamanhoVetor, int rangeDosNumeros){
     Temporizador vet;
+    srand(time(NULL));
     vet.inicio = clock();
     for (unsigned long i = 0; i < tamanhoVetor; i++){
         vetor[i] = rand() % rangeDosNumeros;
@@ -66,7 +67,7 @@ void preenchedorVetor(int *vetor, int tamanhoVetor, int rangeDosNumeros){
     printf("O preenchimento de vetor durou %.5f segundos.\n", ((double)(vet.fim - vet.inicio)) / CLOCKS_PER_SEC);
 }
 
-void executorFuncoes(void funcao(int*, int), int *vetor, int tamanhoVetor, char *nomeFuncao){
+void executorFuncao(void funcao(int*, int), int *vetor, int tamanhoVetor, char *nomeFuncao){
     Temporizador temp;
     temp.inicio = clock();
     funcao(vetor, tamanhoVetor);
@@ -80,23 +81,22 @@ int main(int argc, char const *argv[]){
     void (*pBubble)(int*, int) = &bubbleSort;
     void (*pInsertion)(int*, int) = &insertionSort;
     void (*pSelection)(int*, int) = &selectionSort;
+    const unsigned long tamanhoVetor = 1000000;
+    int vetor[tamanhoVetor], rangeNumeros = 100000;
 
-    const unsigned long tamanhoVetor = 500000;
-    int vetor[tamanhoVetor], rangeNumeros = 10000;
-    srand(time(NULL));
     printf("O vetor contém %ld elementos aleatórios.\n", tamanhoVetor);
 
     preenchedorVetor(vetor, tamanhoVetor, rangeNumeros);
     
-    executorFuncoes(pBubble, vetor, tamanhoVetor, "BubbleSort");
+    executorFuncao(pBubble, vetor, tamanhoVetor, "BubbleSort");
 
     preenchedorVetor(vetor, tamanhoVetor, rangeNumeros);
 
-    executorFuncoes(pInsertion, vetor, tamanhoVetor, "InsertionSort");
+    executorFuncao(pInsertion, vetor, tamanhoVetor, "InsertionSort");
 
     preenchedorVetor(vetor, tamanhoVetor, rangeNumeros);
 
-    executorFuncoes(pSelection, vetor, tamanhoVetor, "SelectionSort");
+    executorFuncao(pSelection, vetor, tamanhoVetor, "SelectionSort");
 
     return 0;
 }
