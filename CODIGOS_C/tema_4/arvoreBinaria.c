d0ab7066738bcc9af08ff53ea9120c7f56f31480
fat83dotcom <fernando.quimic@gmail.com>
2023-01-30 22:19:07 -0300

    Arvore de Busca Binaria - buscar, exibir nó

--- CODIGOS_C/tema_4/arvoreBinaria.c
+++ CODIGOS_C/tema_4/arvoreBinaria.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define true 1
 #define false 0
@@ -39,21 +40,49 @@ Pont criarNo(TIPOCHAVE chave){
     return(newNode);
 }
 
+Pont contem(Pont raiz, TIPOCHAVE chave){
+    if (raiz == NULL) return (NULL);
+    if (raiz->chave == chave) return (raiz);
+    if (raiz->chave > chave) return (contem(raiz->esquerda, chave));
+    return (contem(raiz->direita, chave));
+}
+
+Pont adicionadorNode(int chave, Pont raiz){
+    Pont no = criarNo(chave);
+    raiz = adicionar(raiz, no);
+    return (raiz);
+}
+
+int contadorNodes(Pont raiz){
+    if (!raiz) return 0;
+    return (contadorNodes(raiz->esquerda) + 1 + contadorNodes(raiz->direita));
+}
+
+void exibirArvore(Pont raiz){
+    if (raiz != NULL){
+        printf("%d", raiz->chave);
+        printf("(");
+        exibirArvore(raiz->esquerda);
+        exibirArvore(raiz->direita);
+        printf(")");
+    }
+}
+
 int main(int argc, char const *argv[]){
+    srand(time(NULL));
+    int qtdNodes, randomen, randd;
+    qtdNodes = randomen = 20000;
     Pont raiz = inicializar();
-    Pont no =  criarNo(50);
+    Pont no = criarNo((qtdNodes / 2));
     raiz = adicionar(raiz, no);
-    no =  criarNo(25);
-    raiz = adicionar(raiz, no);
-    no =  criarNo(40);
-    raiz = adicionar(raiz, no);
-    no =  criarNo(15);
-    raiz = adicionar(raiz, no);
-    no =  criarNo(75);
-    raiz = adicionar(raiz, no);
-    no =  criarNo(90);
-    raiz = adicionar(raiz, no);
-    no =  criarNo(55);
-    raiz = adicionar(raiz, no);
+    for (int i = 0; i < qtdNodes; i++){
+        randd = rand() % randomen;
+        raiz = adicionadorNode(randd, raiz);
+    }
+    
+    printf("%d\n", contadorNodes(raiz));
+    exibirArvore(raiz);
+    Pont p = contem(raiz, 15);
+    printf("%p\n", p);
     return 0;
 }
